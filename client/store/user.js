9833a98ed7cecc3e869582a69d2dcffb6501e8bf
Myra Khan <trueimpulse@yahoo.com>
2021-03-08 15:55:58 -0500

    feat(admin): Code cleanup on the api & front end

--- client/store/user.js
+++ client/store/user.js
@@ -57,18 +57,6 @@ export const logout = () => async dispatch => {
   }
 }
 
-// export const sendUserInfo = (userId, info) => {
-//   return async dispatch => {
-//     try {
-//       const {data: user} = await axios.put(`/api/users/${userId}`, info)
-
-//       dispatch(updateUserInfo(user))
-//     } catch (err) {
-//       console.error(err)
-//     }
-//   }
-// }
-
 export const sendUserInfo = info => {
   return async dispatch => {
     try {
