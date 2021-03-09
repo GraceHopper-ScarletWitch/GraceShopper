f833afd2ea330cd268c0f00ec1fbb3b26a13ebfb
Meghan McFarland <meghanmc04@mac.com>
2021-03-09 17:27:10 -0500

    cleaned up code; commented out exports in index.js

--- client/components/index.js
+++ client/components/index.js
@@ -3,5 +3,6 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export {LogIn, SignUp} from './AuthForm'
-export {Checkout} from './checkout-form'
+
+// export {LogIn, SignUp} from './AuthForm'
+// export {Checkout} from './checkout-form'
