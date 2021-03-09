838ab43e113c90db434a87db27efa154e7b2cf52
Meghan McFarland <meghanmc04@mac.com>
2021-03-09 15:04:23 -0500

    feat(checkout) payment info fields added (does not send to db)

--- client/components/CheckoutPage.js
+++ client/components/CheckoutPage.js
@@ -32,35 +32,34 @@ export const Checkout = ({user, isLoggedIn, sendUserInfo}) => {
             Checkout
           </Typography>
 
-          <form name="checkout" onSubmit={handleSubmit}>
-            {/* SHIPPING INFO */}
+          {/* SHIPPING INFO */}
 
-            <Typography variant="h6" gutterBottom>
-              Shipping address
-            </Typography>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  id="name"
-                  name="name"
-                  label="Full name"
-                  fullWidth
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  id="delivery"
-                  name="delivery"
-                  label="Delivery Address"
-                  fullWidth
-                  onChange={handleChange}
-                />
-              </Grid>
+          <Typography variant="h6" gutterBottom>
+            Shipping address
+          </Typography>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                id="name"
+                name="name"
+                label="Full name"
+                fullWidth
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                id="delivery"
+                name="delivery"
+                label="Delivery Address"
+                fullWidth
+                onChange={handleChange}
+              />
+            </Grid>
 
-              {/* <Grid item xs={12} sm={6}>
+            {/* <Grid item xs={12} sm={6}>
                 <TextField
                   required
                   id="city"
@@ -111,16 +110,67 @@ export const Checkout = ({user, isLoggedIn, sendUserInfo}) => {
                   label="Use this address for payment details"
                 />
               </Grid> */}
+          </Grid>
+
+          <Typography variant="h6" gutterBottom>
+            Payment method
+          </Typography>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6}>
+              <TextField
+                required
+                id="cardName"
+                label="Name on card"
+                fullWidth
+                autoComplete="cc-name"
+              />
             </Grid>
-            <Button
-              onClick={handleSubmit}
-              className={classes.button}
-              color="primary"
-              variant="contained"
-            >
-              Submit
-            </Button>
-          </form>
+            <Grid item xs={12} md={6}>
+              <TextField
+                required
+                id="cardNumber"
+                label="Card number"
+                fullWidth
+                autoComplete="cc-number"
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                required
+                id="expDate"
+                label="Expiry date"
+                fullWidth
+                autoComplete="cc-exp"
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                required
+                id="cvv"
+                label="CVV"
+                helperText="Last three digits on signature strip"
+                fullWidth
+                autoComplete="cc-csc"
+              />
+            </Grid>
+            {/* <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox color="secondary" name="saveCard" value="yes" />
+                  }
+                  label="Remember credit card details for next time"
+                />
+              </Grid> */}
+          </Grid>
+
+          <Button
+            onClick={handleSubmit}
+            className={classes.button}
+            color="primary"
+            variant="contained"
+          >
+            Submit
+          </Button>
         </Paper>
       </main>
     </>
